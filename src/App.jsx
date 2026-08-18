62702b96a53df2f0baa2136c4a81a9662e2b8b24
najaandika <naja.andika3000@gmail.com>
2026-08-18 17:05:25 +0700

    Add main landmark for accessibility

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,14 @@ import Footer from './components/Footer';
 export default function App() {
   return (
     <div className="flex w-full max-w-[1440px] mx-auto h-fit flex-col bg-white font-['Poppins'] overflow-clip">
-      <Hero />
-      <Skills />
-      <Education />
-      <Projects />
-      <Certifications />
+      <a href="#main-content" className="skip-link">Skip to main content</a>
+      <main id="main-content" tabIndex="-1">
+        <Hero />
+        <Skills />
+        <Education />
+        <Projects />
+        <Certifications />
+      </main>
       <Footer />
     </div>
   );
